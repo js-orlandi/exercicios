d12ab0c6a5fc7a8b82997b36e7af0cd1bf782e86
Juliano S Orlandi <js.orlandi.dev@gmail.com>
2021-07-23 20:12:14 -0300

    first commit

---
+++ UriExercicios/URI1114SenhaFixa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace URI1114SenhaFixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int senhaCorreta = 2002;
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while(senha != senhaCorreta){
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+
+            if(senha == senhaCorreta){
+                Console.WriteLine("Acesso Permitido");
+            }
+
+
+
+        }
+    }
+}
